6e9d13a41dc3cd66acc7a5842a3c0ddf4601eec7
aremmii <aremmii@gmail.com>
2018-01-29 17:44:43 +0700

    Complete Load Data

--- app/src/main/java/e/promptnow5/liveat500px/manager/http/ApiService.java
+++ app/src/main/java/e/promptnow5/liveat500px/manager/http/ApiService.java
@@ -2,9 +2,8 @@ package e.promptnow5.liveat500px.manager.http;
 
 import e.promptnow5.liveat500px.dao.PhotoItemCollectionDao;
 import retrofit2.Call;
-import retrofit2.http.Multipart;
 import retrofit2.http.POST;
-import retrofit2.http.Part;
+import retrofit2.http.Path;
 
 /**
  * Created by PromptNow5 on 1/29/2018.
@@ -15,12 +14,10 @@ public interface ApiService {
     @POST("list")
     Call<PhotoItemCollectionDao> loadPhotoList();
 
-    @Multipart
     @POST("list/after/{id}")
-    Call<PhotoItemCollectionDao> loadPhotoListAfterId(@Part("id") int id);
+    Call<PhotoItemCollectionDao> loadPhotoListAfterId(@Path("id") int id);
 
-    @Multipart
     @POST("list/before/{id}")
-    Call<PhotoItemCollectionDao> loadPhotoListBeforeId(@Part("id") int id);
+    Call<PhotoItemCollectionDao> loadPhotoListBeforeId(@Path("id") int id);
 
 }
